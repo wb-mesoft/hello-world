4108049a3239d447d10cc52e9e66499e6d4f4778
wb-mesoft <bill@mesoft.org>
2016-07-07 14:01:17 -0400

    test.py
    added test file

---
+++ test.py
@@ -0,0 +1,14 @@
+if __name__ == "__main__":                                                      
+    print("Hello, let's chat!")                                                 
+	print('I am the queen')   
+
+    import random
+    messages=['Hi', 'Hi!', 'How are you?', 'Today is a long day...']
+    num=random.randint(len(messages))
+    print(messages[n])"
+
+    d1=random.randint(1,6)
+    d2=random.randint(1,6)
+    print('D1: {0} D2: {1}'.format(d1, d2)" >> bot.py
+
+    print("die 1:", random.randint(1,6), "die 2:",random.randint(1,6))
